c583e3a8db6e26ec0a19871a60e93574bae2f94a
Ihor Levchenko <ihor224@gmail.com>
2021-06-13 11:01:20 +0300

    сделал дз №3

--- model/index.js
+++ model/index.js
@@ -1,15 +1,66 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const Contact = require("./schemas/contacts");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  try {
+    const data = await Contact.find({});
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const data = await Contact.findOne({ _id: contactId });
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  try {
+    const data = await Contact.findByIdAndRemove({ _id: contactId });
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  try {
+    const data = await Contact.create(body);
+    return data;
+  } catch (error) {
+    next(error);
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  try {
+    const data = await Contact.findOneAndUpdate(
+      { _id: contactId },
+      { ...body },
+      { new: true }
+    );
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const updateStatusContact = async (contactId, body) => {
+  try {
+    const data = await Contact.findOneAndUpdate(
+      { _id: contactId },
+      { ...body },
+      { new: true }
+    );
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +68,5 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+  updateStatusContact,
+};
---
+++ routes/api/validation.js
@@ -0,0 +1,52 @@
+const Joi = require("joi");
+
+const schemaCreateContact = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  phone: Joi.string()
+    .regex(/^\(\d{3}\) \d{3}-\d{4}$/)
+    .required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: false },
+    })
+    .required(),
+});
+
+const schemaUpdateContact = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).optional(),
+  phone: Joi.string()
+    .regex(/^\(\d{3}\) \d{3}-\d{4}$/)
+    .optional(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: false },
+    })
+    .optional(),
+});
+
+const schemaUpdateStatus = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const validate = async (schema, body, next) => {
+  try {
+    await schema.validateAsync(body);
+    next();
+  } catch (err) {
+    next({ status: 400, message: `Field: ${err.message.replace(/"/g, "")}` });
+  }
+};
+
+module.exports.validateCreateContact = (req, _res, next) => {
+  return validate(schemaCreateContact, req.body, next);
+};
+
+module.exports.validateUpdateContact = (req, _res, next) => {
+  return validate(schemaUpdateContact, req.body, next);
+};
+
+module.exports.validateUpdateStatus = (req, _res, next) => {
+  return validate(schemaUpdateStatus, req.body, next);
+};
